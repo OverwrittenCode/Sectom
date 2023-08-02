18f3225c0639e2bf93ae5ecb53805ada6b2f8aeb
OverwrittenCode <kylethapar2007@gmail.com>
2023-08-03 00:16:41 +0100

    Fixed typo (FALSEY -> FALSY) and added constant

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,6 @@
 export const MAX_DESCRIPTION_LENGTH = 2048;
 export const NO_DATA_MESSAGE = "Nothing to view yet in this query selection.";
-export const UNEXPECTED_FALSEY_VALUE__MESSAGE =
-	"Value is an unexpectedly falsey value.";
+export const UNEXPECTED_FALSY_VALUE__MESSAGE =
+	"Value is an unexpectedly falsy value.";
+export const UNEXPECTED_TRUTHY_VALUE_MESSAGE =
+	"Value is an unexpectedly truthy value";
