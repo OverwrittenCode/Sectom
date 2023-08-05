46b5f679eb5ae4d88c7a98759c4b0bd9aed9273e
OverwrittenCode <kylethapar2007@gmail.com>
2023-08-05 22:26:51 +0100

    Transferred functions calling Cases DB to cases.ts
    **Description:**
    - All functions from PaginationButtons that were calling the typegoose CasesModel were moved to cases.ts where they became instance methods to reduce database calls and code efficiency
    - Improved code all together and added minor improvement to code

--- src/models/Moderation/Cases.ts
+++ src/models/Moderation/Cases.ts
@@ -1,5 +1,5 @@
+import { Pagination, PaginationType } from "@discordx/pagination";
 import type {
-	ArraySubDocumentType,
 	DocumentType,
 	Ref,
 	ReturnModelType,
@@ -7,17 +7,67 @@ import type {
 	types
 } from "@typegoose/typegoose";
 import {
-	PropType,
 	getModelForClass,
 	isDocument,
 	prop,
 	queryMethod
 } from "@typegoose/typegoose";
+import {
+	ButtonStyle,
+	Colors,
+	CommandInteraction,
+	ContextMenuCommandInteraction,
+	EmbedBuilder,
+	MessageComponentInteraction
+} from "discord.js";
 
+import { capitalizeFirstLetter } from "../../utils/casing.js";
+import {
+	MAX_DESCRIPTION_LENGTH,
+	UNEXPECTED_FALSY_VALUE__MESSAGE
+} from "../../utils/config.js";
+import { ValidationError } from "../../utils/errors/ValidationError.js";
+import { replyNoData, replyOrFollowUp } from "../../utils/interaction.js";
+import {
+	ButtonIDFormat,
+	TargetClass,
+	TargetClassSingular
+} from "../../utils/ts/Access.js";
+import { ClassPropertyNames, ObjectValues } from "../../utils/ts/General.js";
 import { Server } from "../Server.js";
-import { Action } from "./Action.js";
-import type { ListClassUnion } from "./List.js"
-import { Blacklist,  Whitelist } from "./List.js";
+
+import { Blacklist, Whitelist } from "./List.js";
+
+type Filter = `${TargetClass}`;
+type InputFilters = Filter[];
+type SearchFilter = InputFilters | ["all"];
+type SnowflakeTypeMention = {
+	users: "@";
+	roles: "@&";
+	channels: "#";
+};
+type PaginationData = {
+	id: string;
+	name: string;
+	snowflakeType: Filter;
+};
+type PaginationElement =
+	`<${ObjectValues<SnowflakeTypeMention>}${number}>\`${string}\``;
+
+export type CaseType = Exclude<ClassPropertyNames<Cases>, "server">;
+interface PaginationOptions<T extends SearchFilter> {
+	caseType: CaseType;
+	searchFilter: T;
+}
+
+interface PaginationSenderOptions<T extends SearchFilter>
+	extends PaginationOptions<T> {
+	interaction:
+		| CommandInteraction
+		| MessageComponentInteraction
+		| ContextMenuCommandInteraction;
+	commandName?: string;
+}
 
 interface QueryHelpers {
 	findByCaseNumber: types.AsQueryMethod<typeof findByCaseNumber>;
@@ -41,27 +91,198 @@ function findByCaseNumber(
 @queryMethod(findByCaseNumber)
 export class Cases {
 	@prop({ type: () => Whitelist, default: {} })
-	public whitelist!: SubDocumentType<ListClassUnion>;
+	public whitelist!: SubDocumentType<InstanceType<typeof Whitelist>>;
 
 	@prop({ type: () => Blacklist, default: {} })
-	public blacklist!: SubDocumentType<ListClassUnion>;
+	public blacklist!: SubDocumentType<InstanceType<typeof Blacklist>>;
 
-	@prop({ type: () => [Action], default: [] }, PropType.ARRAY)
-	public actions!: ArraySubDocumentType<Action>[];
+	@prop({ ref: () => Server, unique: true })
+	public readonly server!: Ref<Server>;
 
-	@prop({ ref: () => Server })
-	public server!: Ref<Server>;
-
-	public async addAction(this: DocumentType<Cases>, actionProps: Action) {
-		this.actions.push(actionProps as ArraySubDocumentType<Action>);
-		return await this.save();
+	public createPaginationOption(
+		this: DocumentType<Cases>,
+		prefix: string,
+		type: string,
+		emoji: string
+	) {
+		return {
+			emoji: { name: emoji },
+			id: `${prefix}_${type}`,
+			label: "\u200B",
+			style: ButtonStyle.Secondary
+		};
 	}
 
-	public async removeAction(this: DocumentType<Cases>, caseNumber: number) {
-		this.actions = this.actions.filter(
-			(action) => action.caseNumber != caseNumber
+	public createPaginationOptions(this: DocumentType<Cases>, prefix: string) {
+		return {
+			type: PaginationType.Button,
+			start: this.createPaginationOption(prefix, "beginning", "⏮️"),
+			previous: this.createPaginationOption(prefix, "previous", "◀"),
+			next: this.createPaginationOption(prefix, "next", "▶"),
+			end: this.createPaginationOption(prefix, "end", "⏭"),
+			exit: this.createPaginationOption(prefix, "exit", "❌"),
+			ephemeral: true
+		};
+	}
+
+	public createPaginationButtons<T extends SearchFilter>(
+		this: DocumentType<Cases>,
+		options: PaginationOptions<T>
+	) {
+		const { caseType, searchFilter } = options;
+		const filterArray = searchFilter as Array<Filter | "all">;
+
+		const snowflakePluralType =
+			filterArray[0] == "all" ? "guilds" : filterArray[0];
+
+		const snowflakeSingularType = snowflakePluralType.slice(0, -1) as
+			| `${TargetClassSingular}`
+			| "guild";
+
+		const prefix =
+			`${caseType}_${snowflakeSingularType}_pagination` as ButtonIDFormat<"pagination">;
+
+		return this.createPaginationOptions(prefix);
+	}
+
+	public async PaginateData<T extends SearchFilter>(
+		this: DocumentType<Cases>,
+		options: PaginationSenderOptions<T>
+	) {
+		const { interaction, searchFilter, caseType, commandName } = options;
+
+		if (!interaction.guild || !interaction.guildId)
+			throw new ValidationError(UNEXPECTED_FALSY_VALUE__MESSAGE);
+
+		const data: PaginationData[] = [];
+
+		const filterArray = searchFilter as Array<Filter | "all">;
+		const isAll = filterArray.includes("all");
+		const selection: InputFilters = ["users", "roles", "channels"];
+
+		const keys = isAll
+			? selection
+			: selection.filter((str) => filterArray.includes(str));
+
+		if (commandName) {
+			const { commands } = this[caseType];
+			const matchingCases = keys.filter((key) =>
+				commands.find(
+					(cmd) => cmd.commandName == commandName && cmd[key].length >= 1
+				)
+			);
+			if (matchingCases.length == 0) return await replyNoData(interaction);
+
+			for (const matchingCase of matchingCases) {
+				const cmd = commands.find((cmd) => cmd.commandName == matchingCase);
+				if (!cmd)
+					throw new ValidationError(UNEXPECTED_FALSY_VALUE__MESSAGE);
+
+				const snowflakeArr = cmd[matchingCase].map((v) => {
+					return {
+						id: v.id as string,
+						name: v.name,
+						snowflakeType: matchingCase
+					};
+				});
+
+				data.push(...snowflakeArr);
+			}
+		} else {
+			const matchingCases = keys.filter(
+				(key) => this[caseType][key].length >= 1
+			);
+			if (matchingCases.length == 0) return await replyNoData(interaction);
+
+			for (const matchingCase of matchingCases) {
+				const snowflakeArr = this[caseType][matchingCase].map((v) => {
+					const { id, name } = v;
+					return {
+						id,
+						name,
+						snowflakeType: matchingCase
+					};
+				});
+
+				data.push(...snowflakeArr);
+			}
+		}
+
+		const snowflakeMentionObj: SnowflakeTypeMention = {
+			users: "@",
+			roles: "@&",
+			channels: "#"
+		};
+
+		const mappedData = data.map(
+			(paginationData) =>
+				`<${snowflakeMentionObj[paginationData.snowflakeType]}${
+					paginationData.id
+				}> \`(${paginationData.name})\`` as PaginationElement
 		);
-		return await this.save();
+
+		let embeds: EmbedBuilder[] = [];
+		let description: string = "";
+
+		for (const str of mappedData) {
+			const primaryBreakReason =
+				mappedData.indexOf(str) == mappedData.length - 1;
+			const secondaryBreakReason =
+				description.length + str.length > MAX_DESCRIPTION_LENGTH;
+
+			if (primaryBreakReason || secondaryBreakReason) {
+				if (primaryBreakReason) description += str;
+				const guildName = interaction.guild.name;
+				const CapitaliseSubCommandType = isAll
+					? ""
+					: capitalizeFirstLetter(
+							data[0].snowflakeType.slice(
+								0,
+								-1
+							) as `${TargetClassSingular}`
+					  );
+				const CapitaliseCaseType = capitalizeFirstLetter(caseType);
+
+				const title = `${guildName} ${CapitaliseSubCommandType} ${CapitaliseCaseType}`;
+
+				const embed = new EmbedBuilder()
+					.setTitle(title)
+					.setColor(Colors.Gold)
+					.setDescription(description);
+
+				embeds.push(embed);
+				description == "";
+				break;
+			}
+			description += str;
+		}
+
+		if (embeds.length > 1)
+			embeds = embeds.map((embed, index, arr) =>
+				embed.setFooter({ text: `Page ${index + 1}/${arr.length}` })
+			);
+		else if (embeds.length == 1)
+			return await replyOrFollowUp(interaction, {
+				embeds,
+				ephemeral: true
+			});
+
+		const buttons = this.createPaginationButtons({
+			caseType,
+			searchFilter
+		});
+
+		const paginationEmbeds = embeds.map((embed) => {
+			return {
+				embeds: [embed]
+			};
+		});
+
+		return await new Pagination(
+			interaction,
+			paginationEmbeds,
+			buttons
+		).send();
 	}
 
 	public static async findByServerId(
--- src/utils/components/PaginationButtons.ts
+++ src/utils/components/PaginationButtons.ts
@@ -1,189 +1,19 @@
-import { Pagination, PaginationType } from "@discordx/pagination";
 import {
 	ButtonInteraction,
-	ButtonStyle,
 	Colors,
-	CommandInteraction,
 	EmbedBuilder,
 	GuildMember
 } from "discord.js";
-import lodash from "lodash";
 
-import {
-	ArraySubDocumentType,
-	SubDocumentType
-} from "@typegoose/typegoose/lib/types.js";
-import { Action } from "../../models/Moderation/Action.js";
 import { CasesModel } from "../../models/Moderation/Cases.js";
-import { Command } from "../../models/Moderation/Command.js";
-import { ListClassUnion } from "../../models/Moderation/List.js";
-import { Server } from "../../models/Server.js";
-import {
-	MAX_DESCRIPTION_LENGTH,
-	UNEXPECTED_FALSY_VALUE__MESSAGE
-} from "../config.js";
+import { UNEXPECTED_FALSY_VALUE__MESSAGE } from "../config.js";
 import { ValidationError } from "../errors/ValidationError.js";
 import {
 	getEntityFromGuild,
-	getMentionPrefixFromEntity,
-	replyNoData
+	getMentionPrefixFromEntity
 } from "../interaction.js";
-import type {
-	ButtonIDFormat,
-	CombinedTargetClass,
-	EnumValues,
-	PaginationIDBarrier,
-	ServerModelSelectionSnowflakeType,
-	TargetClass,
-	TargetClassSingular,
-	TargetType
-} from "../ts/Access.js";
+import type { TargetType } from "../ts/Access.js";
 import { AccessListBarrier } from "../ts/Access.js";
-import type { MongooseDocumentType } from "../ts/General.js";
-const { compact } = lodash;
-
-export class PaginationButtons {}
-
-export async function paginateData(
-	data: (Command | ServerModelSelectionSnowflakeType)[],
-	interaction: CommandInteraction
-): Promise<string[]> {
-	const descriptions: string[] = [];
-
-	const addEntryToDescription = async (
-		description: string,
-		info: string,
-		index: number
-	): Promise<{ description: string; index: number } | void> => {
-		const entry = `\`${listIndex}\` ${info}\n`;
-
-		if (description.length + entry.length > MAX_DESCRIPTION_LENGTH) {
-			return; // Reached the limit for this description
-		}
-
-		description += entry;
-		listIndex++;
-		index++;
-
-		return { description, index };
-	};
-
-	const getMention = async (snowflake: ServerModelSelectionSnowflakeType) => {
-		if (interaction.guild?.channels.cache.has(snowflake.id))
-			return `<#${snowflake.id}>`;
-		else if (interaction.guild?.roles.cache.has(snowflake.id))
-			return `<@&${snowflake.id}>`;
-
-		try {
-			return (
-				(await interaction.guild?.members.fetch(snowflake.id)) ||
-				(await interaction.guild?.channels.fetch(snowflake.id)) ||
-				(await interaction.guild?.roles.fetch(snowflake.id))
-			)?.toString();
-		} catch (error) {
-			return "undefined";
-		}
-	};
-
-	let listIndex = 1;
-
-	while (data && data.length > 0) {
-		let description = "";
-		let index = 0;
-		let info = "";
-
-		while (index < data.length) {
-			if (data[index] instanceof Command) {
-				const command = data[index] as Command;
-
-				const groups = [
-					command.users,
-					command.roles,
-					command.channels
-				].filter(
-					async (v, i) => typeof (await getMention(v[i])) != "undefined"
-				);
-
-				for (const group of groups) {
-					for (const item of group) {
-						info = `\`${command.commandName}\`: ${item.name} (${item.id})`;
-
-						const result = await addEntryToDescription(
-							description,
-							info,
-							index
-						);
-						if (!result) break;
-
-						description = result.description;
-						index = result.index;
-					}
-				}
-			} else {
-				const snowflake = data[index] as ServerModelSelectionSnowflakeType;
-
-				const snowflakeMention = await getMention(snowflake);
-				if (snowflakeMention) info = snowflakeMention;
-
-				const result = await addEntryToDescription(
-					description,
-					info,
-					index
-				);
-				if (!result) {
-					break; // Reached the limit for this description
-				}
-
-				description = result.description;
-				index = result.index;
-			}
-
-			if (description.length + info.length > MAX_DESCRIPTION_LENGTH) {
-				break;
-			}
-		}
-
-		descriptions.push(description);
-		data = data.slice(index);
-	}
-
-	return descriptions;
-}
-
-export function createPaginationButtons(
-	accessListType: `${PaginationIDBarrier}`,
-	targetTypePlural: TargetClass | "guilds" = "guilds"
-) {
-	const targetTypeSingular = targetTypePlural.slice(
-		0,
-		-1
-	) as TargetClassSingular;
-	const prefix =
-		`${accessListType}_${targetTypeSingular}_pagination` as ButtonIDFormat<"pagination">;
-
-	return createPaginationOptions(prefix);
-}
-
-function createPaginationOption(prefix: string, type: string, emoji: string) {
-	return {
-		emoji: { name: emoji },
-		id: `${prefix}_${type}`,
-		label: "\u200B",
-		style: ButtonStyle.Secondary
-	};
-}
-
-function createPaginationOptions(prefix: string) {
-	return {
-		type: PaginationType.Button,
-		start: createPaginationOption(prefix, "beginning", "⏮️"),
-		previous: createPaginationOption(prefix, "previous", "◀"),
-		next: createPaginationOption(prefix, "next", "▶"),
-		end: createPaginationOption(prefix, "end", "⏭"),
-		exit: createPaginationOption(prefix, "exit", "❌"),
-		ephemeral: true
-	};
-}
 
 export async function ButtonComponentMoveSnowflake(
 	interaction: ButtonInteraction
@@ -259,7 +89,7 @@ export async function ButtonComponentMoveSnowflake(
 	const confirmedEmbed = new EmbedBuilder()
 		.setTitle("Success")
 		.setDescription(
-			`${targetMention} has been moved from the ${oppositeList} to the ${list}	${
+			`${targetMention} has been moved from the ${oppositeList} to the ${list} ${
 				commandName ?? "guild"
 			}`
 		)
@@ -272,93 +102,4 @@ export async function ButtonComponentMoveSnowflake(
 		embeds: [confirmedEmbed],
 		components: []
 	});
-}
-
-function isListType(
-	caseDocument:
-		| SubDocumentType<ListClassUnion>
-		| ArraySubDocumentType<Action>[]
-): caseDocument is SubDocumentType<ListClassUnion> {
-	return !Array.isArray(caseDocument);
-}
-
-export async function PaginationSender(params: {
-	server: MongooseDocumentType<Server>;
-	list: `${PaginationIDBarrier}`;
-	snowflakePluralType: EnumValues<typeof CombinedTargetClass>;
-	interaction: CommandInteraction;
-	commandName?: string;
-}) {
-	const { server, list, snowflakePluralType, commandName, interaction } =
-		params;
-
-	if (!interaction.guild || !interaction.guildId)
-		throw new ValidationError(UNEXPECTED_FALSY_VALUE__MESSAGE);
-
-	const cases = await CasesModel.findByServerId(interaction.guildId);
-	if (!cases) throw new ValidationError(UNEXPECTED_FALSY_VALUE__MESSAGE);
-
-	let data: string[] = [];
-	const accessList = cases[list];
-
-	if (isListType(accessList)) {
-		if (commandName)
-			data = await paginateData(
-				accessList.commands.filter((v) => v.commandName == commandName),
-				interaction
-			);
-		else
-			data = await paginateData(
-				snowflakePluralType == "guilds"
-					? [
-							...accessList.channels,
-							...accessList.roles,
-							...accessList.users
-					  ]
-					: accessList[snowflakePluralType],
-				interaction
-			);
-	} else {
-		const actionObjects = compact(
-			accessList.map((action) => {
-				if (action.caseNumber)
-					return {
-						id: action.caseNumber.toString(),
-						name: `Case #${action.caseNumber} - Type: ${
-							action.type
-						} - Executor: ${action.executor.id} - Target: ${
-							action.target.id
-						} - Reason: ${action.reason || "No reason provided"}`
-					};
-			})
-		);
-
-		data = await paginateData(actionObjects, interaction);
-	}
-
-	if (data.length == 0) return await replyNoData(interaction);
-
-	const pages = data.map((d, i) => {
-		return {
-			embeds: [
-				new EmbedBuilder()
-					.setTitle(`${interaction.guild!.name} Cases`)
-					.setDescription(d)
-					.setColor("#e6c866")
-					.setFooter({
-						text: `Page ${i + 1}/${data.length}`
-					})
-			]
-		};
-	});
-
-	const buttons = createPaginationButtons(list, snowflakePluralType);
-
-	if (pages.length == 1)
-		return await interaction.reply({
-			embeds: pages[0].embeds,
-			ephemeral: true
-		});
-
-	return await new Pagination(interaction, pages, buttons).send();
 }
