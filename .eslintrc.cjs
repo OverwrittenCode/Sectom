52f847f06e1a4199f89821812a3ad837e6880306
OverwrittenCode <kylethapar2007@gmail.com>
2023-08-05 11:58:04 +0100

    disabled eslint error "no-duplicate-enum-values"

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
 		"@typescript-eslint/ban-types": "off",
 		"@typescript-eslint/no-unused-vars": "off",
 		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/no-duplicate-enum-values": "off",
 		"comma-dangle": "off",
 		indent: "off",
 		"linebreak-style": ["error", "unix"],
