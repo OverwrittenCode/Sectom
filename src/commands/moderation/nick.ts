1a9156449d80170af69436f63bf3642527e10dab
OverwrittenCode <kylethapar2007@gmail.com>
2024-04-19 18:18:20 +0100

    chore: update imports

--- src/commands/moderation/nick.ts
+++ src/commands/moderation/nick.ts
@@ -3,7 +3,7 @@ import { Category, RateLimit, TIME_UNIT } from "@discordx/utilities";
 import { ReasonSlashOption } from "@helpers/decorators/slashOptions/reason.js";
 import { TargetSlashOption } from "@helpers/decorators/slashOptions/target.js";
 import { BotRequiredPermissions } from "@helpers/guards/BotRequiredPermissions.js";
-import { ActionModerationManager } from "@models/framework/manager/ActionModerationManager.js";
+import { ActionModerationManager } from "@managers/ActionModerationManager.js";
 import { CaseActionType, EntityType } from "@prisma/client";
 import { COMMAND_CATEGORY } from "@ts/enums/COMMAND_CATEGORY.js";
 import { COMMAND_SLASH_OPTION_TARGET_FLAGS } from "@ts/enums/COMMAND_SLASH_OPTION_TARGET_FLAGS.js";
