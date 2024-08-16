00b401403a3578479913cf3921ad231c841a34ae
OverwrittenCode <kylethapar2007@outlook.com>
2024-08-16 01:40:57 +0100

    build: update dependencies

--- src/helpers/decorators/slash/autocomplete.ts
+++ src/helpers/decorators/slash/autocomplete.ts
@@ -1,12 +1,68 @@
 import { EnumChoice } from "@discordx/utilities";
+import { EventType } from "@prisma/client";
+import { type ApplicationCommandOptionChoiceData, ApplicationCommandOptionType } from "discord.js";
 import { type ParameterDecoratorEx, SlashChoice, type SlashOptionAutoCompleteOptions } from "discordx";
+
 import { MAX_AUTOCOMPLETE_OPTION_LIMIT } from "~/constants.js";
 import { ValidationError } from "~/helpers/errors/ValidationError.js";
 import { CommandUtils } from "~/helpers/utils/command.js";
 import { ObjectUtils } from "~/helpers/utils/object.js";
+import { StringUtils } from "~/helpers/utils/string.js";
+import { ActionManager } from "~/models/framework/managers/ActionManager.js";
+import type { Typings } from "~/ts/Typings.js";
+
+import type { ActionType } from "@prisma/client";
+
 type AutoCompleteSlashOptionOptions = SlashOptionAutoCompleteOptions<Lowercase<string>, string>;
 
 type AutocompleteValue = NonNullable<Typings.SlashOptionTransformerValueParam<AutoCompleteSlashOptionOptions>>;
+
+interface ActionChoiceSlashOptionOptions extends Partial<Pick<AutoCompleteSlashOptionOptions, "description" | "name">> {
+	eventType: EventType;
+	grouped?: boolean;
+	required?: boolean;
+}
+
+export function ActionSlashChoiceOption(options: ActionChoiceSlashOptionOptions): ParameterDecoratorEx {
+	const {
+		eventType,
+		required,
+		grouped = true,
+		description = `The ${eventType} log type group`,
+		name = `${eventType.toLowerCase()}_log_type` as Lowercase<string>
+	} = options;
+
+	const glossary = choiceConfigs[eventType].types.reduce(
+		(acc, type) => {
+			let formattedKey: string = type;
+
+			if (grouped) {
+				formattedKey = type.replace(choiceConfigs[eventType].regex, "");
+			}
+
+			formattedKey = StringUtils.concatenate(
+				" ",
+				...formattedKey.toLowerCase().split("_").map(StringUtils.capitaliseFirstLetter)
+			);
+
+			acc[formattedKey] = type;
+
+			return acc;
+		},
+		{} as Record<string, ActionType>
+	);
+
+	return AutoCompleteSlashChoiceOption(
+		{
+			description,
+			name,
+			type: ApplicationCommandOptionType.String,
+			required
+		},
+		glossary
+	);
+}
+
 export function AutoCompleteSlashChoiceOption(
 	options: AutoCompleteSlashOptionOptions,
 	glossary: AutocompleteValue[] | Record<AutocompleteValue, AutocompleteValue>
@@ -74,3 +130,13 @@ export function AutoCompleteSlashChoiceOption(
 	});
 }
 
+const choiceConfigs = {
+	[EventType.BOT]: {
+		types: ActionManager.createBasedTypes,
+		regex: StringUtils.regexes.createBasedActionModifiers
+	},
+	[EventType.DISCORD]: {
+		types: ActionManager.eventBasedLogTypes[EventType.DISCORD],
+		regex: StringUtils.regexes.discordBasedActionLog
+	}
+};
